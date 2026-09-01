c2c625f7f7121bfe3436e0b4c92c557db44413a2
charles <charlesjack8910@gmail.com>
2026-09-01 12:14:29 +0100

    i've updated the code

---
+++ index.js
@@ -0,0 +1,17 @@
+document.getElementById("weather").addEventListener("click" , function(e) {
+    e.preventDefault();
+
+    window.open("weather.html","_blank") 
+} );
+document.getElementById("task").addEventListener("click" , function(e) {
+    e.preventDefault();
+
+    window.open("task.html","_blank")
+} );
+document.getElementById("port").addEventListener("click" , function(e) {
+    e.preventDefault();
+
+    window.open("lio.html","_blank"); 
+} );
+
+
